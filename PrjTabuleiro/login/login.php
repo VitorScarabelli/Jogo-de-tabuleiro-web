80503eeaa78e58f7e8f6652b677eff004450f225
Liv288 <liveira026@gmail.com>
2025-09-14 11:59:54 -0300

    Update login.php

--- PrjTabuleiro/login/login.php
+++ PrjTabuleiro/login/login.php
@@ -7,27 +7,21 @@
     <title>Login</title>
 </head>
 <body>
-    <header>
-        <div class="left">
-            <a href="../Index/index.php"><img src="./KitComposiçãoLogin/svg/Botões/BackButton.svg" alt="" width="70px" height="70px"></img></a>
-            <a href="../Index/index.php"><img src="./KitComposiçãoLogin/svg/Botões/HomeButton.svg" alt="" width="70px" height="70px"></img></a>
-        </div>
-        <div class="right">
-<<<<<<< HEAD
-            <a href="#"><img src="../KitComposiçãoLogin/svg/Botões/MusicButton.svg" alt="" width="70px" height="70px"></img></a>
-            <a href="#"><img src="../KitComposiçãoLogin/svg/Botões/ConfigButton.svg" alt="" width="70px" height="70px"></img></a>
-=======
-            <a href="#"><img src="./KitComposiçãoLogin/svg/Botões/MusicButton.svg" alt="" width="70px" height="70px"></img></a>
-            <a href="#"><img src="./KitComposiçãoLogin/svg/Botões/ConfigButton.svg" alt="" width="70px" height="70px"></img></a>
->>>>>>> 9aecc831cc8fec0da1ad0424015809e87237437c
-        </div>
-    </header>
+    <img src="./KitComposiçãoLogin/svg/CéuTardio.svg" alt="" class="background">
+    <div class="left">
+        <a href="../Index/index.php"><img src="./KitComposiçãoLogin/svg/Botões/BackButton.svg" alt="" width="70px" height="70px"></img></a>
+        <a href="../Index/index.php"><img src="./KitComposiçãoLogin/svg/Botões/HomeButton.svg" alt="" width="70px" height="70px"></img></a>
+    </div>
+    <div class="right">
+        <a href="#"><img src="./KitComposiçãoLogin/svg/Botões/MusicButton.svg" alt="" width="70px" height="70px"></img></a>
+        <a href="#"><img src="./KitComposiçãoLogin/svg/Botões/ConfigButton.svg" alt="" width="70px" height="70px"></img></a>
+    </div>
     <div class="container">
-        <h1 class="titulo">Login</h1>
-        <div class="quadrados">
+        <img src="./KitComposiçãoLogin/svg/LivroLogin.svg" alt="" class="img-livro">
+        <h1 class="titulo">LOGIN</h1>
             <form method="get" class="formulario">
                 <label for="" class="email">Email:</label>
-                <div class="primeiraCaixaTexto">
+                <div class="caixaTexto">
                     <input type="email" name="email" id="email" placeholder="@gmail.com">
                 </div>
                 <br />
@@ -36,16 +30,13 @@
                     <input type="password" name="senha" id="senha" placeholder="password">
                 </div>
                 <br />
-                <div class="caixaBotao">
-                    <button type="submit" class="botao">CONFIRMAR</button>
+                <div class="caixa-btn">
+                    <button type="submit" class="btn">CONFIRMAR</button>
                 </div>
             </form>
+
             <div class="links">
-<<<<<<< HEAD
-                <p class="textCadastro">Não possui uma conta ainda?</p> <a href="#">Criar uma</a>
-=======
-                <p class="textCadastro">Não possui uma conta ainda?</p> <a href="../cadastro/index/index.php">Criar uma</a>
->>>>>>> 9aecc831cc8fec0da1ad0424015809e87237437c
+                <p class="text-cadastro">Não possui uma conta ainda?</p> <a href=".././cadastro/index/index.php" class="link-cadastro">Criar uma</a>
             </div>
         </div>
     </div>
