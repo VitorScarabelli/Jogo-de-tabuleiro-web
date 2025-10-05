00b98eae6691378e3634f9f66edffeccad1e3d5a
Vitor Scarabelli <scarabelli.vitinho@gmail.com>
2025-10-04 21:16:03 -0300

    Delete PrjTabuleiro directory

--- PrjTabuleiro/includes/verificacao_session_usu.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    session_start();
-    if(!isset($_SESSION['autorizacao']) == true){
-        unset($_SESSION['autorizacao']);
-        session_destroy();
-        header('Location: ../login/login.php');
-    }
-?>
